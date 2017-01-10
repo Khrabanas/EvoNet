cc3ef7bfd24006696f100d84e7b60412a7db18f7
Corvus Corax <cthulhuborn@gmail.com>
2017-01-10 13:55:01 -0800

    Create temp.js

---
+++ temp.js
@@ -0,0 +1,32 @@
+var temp = 14;
+
+//the tempature variation per season
+var seasons = {
+  summer: {
+    plus: ,
+    minus: ,
+  },
+  
+  fall: {
+    plus: ,
+    minus: ,
+  },
+  
+  winter: {
+    plus: ,
+    minus: ,
+  },
+  
+  spring: {
+    plus: ,
+    minus: ,
+  },
+}
+//14 --> 57
+
+
+
+
+
+
+
