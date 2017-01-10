4c275fbd6bcd3272ad97cc0d8e39a2f3716632e1
Corvus Corax <cthulhuborn@gmail.com>
2017-01-10 14:03:28 -0800

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,7 +1,10 @@
-var temp = 14;
+//var temp = 14;
+var tempHigh = [12.1,	15.7,	18.4,	21.7,	26.6,	30.6, 33.6,	32.9,	30.7,	25.3,	17.7,	12.2];
+//jan to dec
+var tempLow = [3.8, 5.2, 6.7,	7.9, 10.6, 13.2, 14.6, 14.4, 13.2, 10.1, 63.6];
 
 //the tempature variation per season
-var seasons = {
+/*var seasons = {
   summer: {
     plus: ,
     minus: ,
@@ -21,7 +24,7 @@ var seasons = {
     plus: ,
     minus: ,
   },
-}
+} */
 //14 --> 57
 
 
