b1136a9f36881424e4a3a1fa517a0665e962c2d5
Corvus Corax <cthulhuborn@gmail.com>
2017-01-10 14:05:00 -0800

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,4 +1,6 @@
 //var temp = 14;
+
+//sacremento http://www.usclimatedata.com/climate/california/united-states/3174#
 var tempHigh = [12.1,	15.7,	18.4,	21.7,	26.6,	30.6, 33.6,	32.9,	30.7,	25.3,	17.7,	12.2];
 //jan to dec
 var tempLow = [3.8, 5.2, 6.7,	7.9, 10.6, 13.2, 14.6, 14.4, 13.2, 10.1, 63.6];
