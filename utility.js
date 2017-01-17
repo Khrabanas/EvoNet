2e61f1f8ec172ebfead53da8abf1788bb148f8d4
Corvus Corax <cthulhuborn@gmail.com>
2017-01-17 13:33:08 -0800

    attempting to get data from each click.

--- utility.js
+++ utility.js
@@ -1,8 +1,28 @@
 
-document.onclick = function(e){
-
+function cursorPos() {
     cursorX = e.pageX;
     cursorY = e.pageY;
+}
+
+var selected = {org: [], plants: [],};
+function clickData() {
+    selected = {org: [], plants: [],};
+    for(i=0; i < pop.length; i++) {
+        if (findDis(cursorX, cursorY, pop[i].x, pop[i].y) < pop[i].radius * pop[i].radius) {
+            selected.org[selected.org.length] = pop[i];
+        }
+    }
+}
+function findDis(x1,y1,x2,y2) {
+  return (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
+  //requires a Math.sqrt to get actual value. this can be found in the getProx function.
+}
+
+
+document.onclick = function(e){
+    
+    cursorPos();
+    clickData();
     
     render();
     
